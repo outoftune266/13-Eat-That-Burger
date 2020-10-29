9dc5302a40df6085362c70a95fc6c2175a17551b
Zak Monnet <zakmonnet@gmail.com>
2020-10-29 14:10:50 -0500

    added intial files to repo. connection.js and orm.js have been added with initial logic

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+const connection = require("../config/connection.js");
+
+let orm = {
+
+    selectAll: function(tableInput, cb) {
+        connection.query("SELECT * FROM ?", [tableInput], function(err,result) {
+            if (err) throw err;
+
+             cb(result);
+        });
+    },
+
+    insertOne: function(tableInput, cols, name, state, cb) {
+        connection.query("INSERT INTO ? (??) VALUES (??,??)", [tableInput, cols, name, state], function(err, result) {
+            if (err) throw err;
+
+            cb(result);
+        });
+    },
+
+    updateOne: function(tableInput, cols, ids) {
+        connection.query("UPDATE ? SET ?? WHERE ??", [tableInput, cols, ids], function(err, result) {
+            if (err) throw err;
+
+            cb(result);
+        });
+    };
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+    VALUES  ("Cheesburger", False),
+            ("Black Bean Burger", False),
+            ("Mushroom Swiss Burger", False),
+            ("Loco Moco", False),
+            ("Cheddar Jalepeno Burger", False);
