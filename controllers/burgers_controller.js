a1f0f45b04ed7b543ebc6c6ec3860f8786545f48
Zak Monnet <zakmonnet@gmail.com>
2020-10-30 13:23:10 -0500

    edited html views. corrected error in burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cat = require("../../../vu-nsh-fsf-pt-07-2020-u-c/13-MVC/01-Activities/17-CatsApp/Solved/models/cat.js");
 const burger = require("../models/burger.js");
 
 const router = express.Router();
